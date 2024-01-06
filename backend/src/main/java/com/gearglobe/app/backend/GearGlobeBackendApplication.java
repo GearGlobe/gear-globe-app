252b2ffe1aabb761e304a4485e589cc4acad092f
Filip Krzymiński <filipkrzym@gmail.com>
2024-01-06 21:25:01 +0100

    init

---
+++ backend/src/main/java/com/gearglobe/app/backend/GearGlobeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gearglobe.app.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GearGlobeBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GearGlobeBackendApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/gearglobe/app/backend/GearGlobeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gearglobe.app.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GearGlobeBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
